8163adf151196c1ea2902b80213b00f6dede546e
Kacper Bielecki <kazjote@gmail.com>
2013-03-17 21:54:16 +0100

    Formatting

--- extension.js
+++ extension.js
@@ -8,20 +8,20 @@ const Gio = imports.gi.Gio;
 let entry, dialog, button, shown;
 
 function _hideHello() {
-    Main.uiGroup.remove_actor(entry);
-    dialog.close();
-    dialog = null;
-    entry  = null;
+  Main.uiGroup.remove_actor(entry);
+  dialog.close();
+  dialog = null;
+  entry  = null;
 }
 
 function _showHello() {
   if (!entry) {
-      entry = new St.Entry({ name:        "newTask",
-                             hint_text:   "New task...",
-                             track_hover: true,
-                             style_class: 'task-entry' });
+    entry = new St.Entry({ name:        "newTask",
+                           hint_text:   "New task...",
+                           track_hover: true,
+                           style_class: 'task-entry' });
 
-      global.log("Adding entry");
+    global.log("Adding entry");
   }
 
   if (!dialog) {
@@ -53,23 +53,23 @@ function _showHello() {
 }
 
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
+  button = new St.Bin({ style_class: 'panel-button',
+                        reactive: true,
+                        can_focus: true,
+                        x_fill: true,
+                        y_fill: false,
+                        track_hover: true });
+  let icon = new St.Icon({ icon_name: 'system-run-symbolic',
+                           style_class: 'system-status-icon' });
 
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
+  button.set_child(icon);
+  button.connect('button-press-event', _showHello);
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+  Main.panel._rightBox.insert_child_at_index(button, 0);
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+  Main.panel._rightBox.remove_child(button);
 }
