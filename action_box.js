61a868d5d7a1c59cc6829296a99840fb09b9b3ff
Kacper Bielecki <kazjote@gmail.com>
2014-02-02 22:49:00 +0100

    Extracted input entries creation to action_box

---
+++ action_box.js
@@ -0,0 +1,137 @@
+const St = imports.gi.St;
+const Clutter = imports.gi.Clutter;
+const Lang = imports.lang;
+const ExtensionUtils = imports.misc.extensionUtils;
+
+const Me = ExtensionUtils.getCurrentExtension();
+const HelpDialog = Me.imports.help_dialog;
+
+const ActionBox = new Lang.Class({
+    Name: 'ActionBox',
+
+    _init: function(layout, trayMenu, authenticator, rtm, taskList, notifier) {
+        this._layout = layout;
+        this._authenticator = authenticator;
+        this._rtm = rtm;
+        this._taskList = taskList;
+        this._trayMenu = trayMenu;
+        this._notifier = notifier;
+    },
+
+    setup: function() {
+        let taskEntry = this._setupNewTask();
+        let searchEntry = this._setupSearch();
+
+        this._setupHelp();
+
+        this._trayMenu.actor.connect('key-release-event', function(object, event) {
+            let modifiers = event.get_state();
+
+            if (modifiers & Clutter.ModifierType.CONTROL_MASK) {
+                if(event.get_key_symbol() == Clutter.n) {
+                    taskEntry.grab_key_focus();
+                }
+
+                if(event.get_key_symbol() == Clutter.f) {
+                    searchEntry.grab_key_focus();
+                }
+            }
+        });
+
+        this._trayMenu.connect('open-state-changed', function(self, open) {
+            if(open) {
+                taskEntry.grab_key_focus();
+            }
+        });
+    },
+
+    _setupNewTask: function() {
+        let label = new St.Label({ name: 'newTaskLabel',
+                                   style_class: 'task-label',
+                                   text: "New task" });
+
+        let entry = new St.Entry({ name: 'newTask',
+                                   hint_text: "New task...",
+                                   track_hover: true,
+                                   style_class: 'task-entry' });
+
+        this._layout.add(label, {row: 0, col: 0, x_expand: false});
+        this._layout.add(entry, {row: 0, col: 1, x_expand: true, x_fill: true, y_fill: false, y_expand: false});
+
+        entry.connect('key-release-event', Lang.bind(this, function(object, event) {
+            let symbol = event.get_key_symbol();
+
+            if(symbol == Clutter.Return) {
+                let text = entry.get_text();
+
+                this._authenticator.authenticated(Lang.bind(this, function() {
+                    this._addEntry(text);
+                }));
+
+                entry.set_text('');
+            }
+        }));
+
+        return entry;
+    },
+
+    _setupSearch: function() {
+        let label = new St.Label({
+              name: 'searchTaskLabel',
+              style_class: 'task-label',
+              text: "Search" });
+
+        let entry = new St.Entry({
+              name: 'search',
+              hint_text: "Search query...",
+              track_hover: true,
+              style_class: 'task-entry' });
+
+        this._layout.add(label, {row: 1, col: 0, x_expand: false});
+        this._layout.add(entry, {row: 1, col: 1, x_expand: true, x_fill: true, y_fill: false, y_expand: false});
+
+        entry.connect('key-release-event', Lang.bind(this, function(object, event) {
+            if(event.get_key_symbol() == Clutter.Return) {
+                this._taskList.refresh(entry.text);
+                entry.text = '';
+            }
+        }));
+
+        return entry;
+    },
+
+    _setupHelp: function() {
+        let helpIcon = St.Icon.new();
+        let iconButton = St.Button.new();
+
+        iconButton.child = helpIcon;
+
+        this._layout.add(iconButton, { row: 0, col: 2, x_expand: false });
+
+        helpIcon.icon_name = 'dialog-question-symbolic';
+        helpIcon.icon_size = 16;
+        helpIcon.style_class = 'help-icon';
+
+        iconButton.connect('clicked', Lang.bind(this, function() {
+            let dialog = new HelpDialog.HelpDialog();
+            dialog.open();
+            this._trayMenu.close();
+        }));
+    },
+
+    _addEntry: function(content) {
+        this._rtm.get('rtm.timelines.create', {}, Lang.bind(this, function(resp) {
+            let options = { timeline: resp.rsp.timeline, name: content, parse: 1 };
+            this._rtm.get('rtm.tasks.add', options, Lang.bind(this, function(resp) {
+                if (resp.rsp.stat == 'ok') {
+                    this._notifier.notify("Task successfully created", content);
+                    this._taskList.refresh();
+                } else {
+                    this._notifier.notify("Failed to create the task", content);
+                }
+            }));
+        }));
+    }
+});
+
+// vim: ts=4 sw=4
--- extension.js
+++ extension.js
@@ -16,27 +16,13 @@ const Rtm           = Me.imports.rtm;
 const Authenticator = Me.imports.authenticator;
 const Notifier      = Me.imports.notifier;
 const TaskList      = Me.imports.task_list;
-const HelpDialog    = Me.imports.help_dialog;
 const DBusService   = Me.imports.dbus_service.DBusService;
+const ActionBox     = Me.imports.action_box.ActionBox;
 
 const AppKey    = '7dfc8cb9f7985d712e355ee4526d5c88';
 const AppSecret = '5792b9b6adbc3847';
 
 let rtm, dbusService, authenticator, notifier, tray, taskList;
-
-function _addEntry(content) {
-    rtm.get('rtm.timelines.create', {}, function(resp) {
-        let options = { timeline: resp.rsp.timeline, name: content, parse: 1 };
-        rtm.get('rtm.tasks.add', options, function(resp) {
-            if (resp.rsp.stat == 'ok') {
-                notifier.notify("Task successfully created", content);
-                taskList.refresh();
-            } else {
-                notifier.notify("Failed to create the task", content);
-            }
-        });
-    });
-}
 
 function enable() {
     var theme = imports.gi.Gtk.IconTheme.get_default();
@@ -68,58 +54,12 @@ function enable() {
 
     let table_layout = new St.Table();
 
-    let label = new St.Label({ name: 'newTaskLabel',
-                               style_class: 'task-label',
-                               text: "New task" });
-
-    let entry = new St.Entry({ name: 'newTask',
-                               hint_text: "New task...",
-                               track_hover: true,
-                               style_class: 'task-entry' });
-
-    table_layout.add(label, {row: 0, col: 0, x_expand: false});
-    table_layout.add(entry, {row: 0, col: 1, x_expand: true, x_fill: true, y_fill: false, y_expand: false});
-
-    entry.connect('key-release-event', function(object, event) {
-        let symbol = event.get_key_symbol();
-
-        if(symbol == Clutter.Return) {
-            let text = entry.get_text();
-
-            authenticator.authenticated(function() {
-                _addEntry(text);
-            });
-
-            entry.set_text('');
-        }
-    });
-
-    tray.menu.connect('open-state-changed', function(self, open) {
-        if(open) {
-            entry.grab_key_focus();
-        }
-    });
-
-    label = new St.Label({
-          name: 'searchTaskLabel',
-          style_class: 'task-label',
-          text: "Search" });
-
-    let searchEntry = new St.Entry({
-          name: 'search',
-          hint_text: "Search query...",
-          track_hover: true,
-          style_class: 'task-entry' });
-
     let processingLabel = new St.Label({
           name: 'processingLabel',
           style_class: 'processing-label' });
 
     rtm = new Rtm.RememberTheMilk(AppKey, AppSecret, 'write', processingLabel);
     authenticator = new Authenticator.RtmAuthenticator(rtm, notifier);
-
-    table_layout.add(label, {row: 1, col: 0, x_expand: false});
-    table_layout.add(searchEntry, {row: 1, col: 1, x_expand: true, x_fill: true, y_fill: false, y_expand: false});
 
     let processingLabelShown = false;
 
@@ -137,21 +77,10 @@ function enable() {
         }
     };
 
-    let helpIcon = St.Icon.new();
-    let iconButton = St.Button.new();
-    iconButton.child = helpIcon;
+    taskList = new TaskList.TaskList(tray.menu, authenticator, rtm, notifier);
 
-    table_layout.add(iconButton, { row: 0, col: 2, x_expand: false });
-
-    helpIcon.icon_name = 'dialog-question-symbolic';
-    helpIcon.icon_size = 16;
-    helpIcon.style_class = 'help-icon';
-
-    iconButton.connect('clicked', function() {
-        let dialog = new HelpDialog.HelpDialog();
-        dialog.open();
-        tray.menu.close();
-    });
+    let actionBox = new ActionBox(table_layout, tray.menu, authenticator, rtm, taskList, notifier);
+    actionBox.setup();
 
     let main_box = new St.BoxLayout({vertical: true});
 
@@ -160,29 +89,6 @@ function enable() {
     let menu_item = new PopupMenu.PopupBaseMenuItem({reactive: false});
 
     menu_item.addActor(main_box, {expand: true});
-
-    taskList = new TaskList.TaskList(tray.menu, authenticator, rtm, notifier);
-
-    tray.menu.actor.connect('key-release-event', function(object, event) {
-        let modifiers = event.get_state();
-
-        if (modifiers & Clutter.ModifierType.CONTROL_MASK) {
-            if(event.get_key_symbol() == Clutter.n) {
-                entry.grab_key_focus();
-            }
-
-            if(event.get_key_symbol() == Clutter.f) {
-                searchEntry.grab_key_focus();
-            }
-        }
-    });
-
-    searchEntry.connect('key-release-event', function(object, event) {
-        if(event.get_key_symbol() == Clutter.Return) {
-            taskList.refresh(searchEntry.text);
-            searchEntry.text = '';
-        }
-    });
 
     tray.menu.addMenuItem(menu_item);
 }
