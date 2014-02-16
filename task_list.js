847ef439193bf9d35261db25520fedc693889c58
Kacper Bielecki <kazjote@gmail.com>
2014-02-16 19:13:20 +0100

    More styling fixes

--- task_list.js
+++ task_list.js
@@ -1,7 +1,7 @@
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
 const Lang = imports.lang;
 const St = imports.gi.St;
-const GLib = imports.gi.GLib;
-const Gio = imports.gi.Gio;
 
 const PopupMenu = imports.ui.popupMenu;
 
